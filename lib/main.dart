9c92ab49d73a7fb48555922d01ccfd82e79ef82e
imtiaz <programmer.imtiaz@gmail.com>
2020-04-17 11:20:40 +0600

    UPDATE- functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String output = "0";
+  String _output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operand = "";
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +42,56 @@ class _MyHomePageState extends State<MyHomePage> {
 
   buttonPressed(String buttonText) {
     print(buttonText);
+
+    if (buttonText == "CLEAR") {
+      _output = "0";
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "/" ||
+        buttonText == "X") {
+      num1 = double.parse(output);
+
+      operand = buttonText;
+
+      _output = "0";
+    } else if (buttonText == ".") {
+      if (_output.contains(".")) {
+        print("Already Contains a decimal");
+        return;
+      } else {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "=") {
+      num2 = double.parse(output);
+
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      }
+      if (operand == "-") {
+        _output = (num1 - num2).toString();
+      }
+      if (operand == "/") {
+        _output = (num1 / num2).toString();
+      }
+      if (operand == "X") {
+        _output = (num1 * num2).toString();
+      }
+
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+
+    print(_output);
+
+    setState(() {
+      output = double.parse(_output).toStringAsPrecision(2);
+    });
   }
 
   Widget buildButton(String buttonText) {
@@ -53,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         color: Colors.blueAccent,
         textColor: Colors.black,
-        onPressed: () => {buttonPressed(buttonText)},
+        onPressed: () => buttonPressed(buttonText),
       ),
     );
   }
